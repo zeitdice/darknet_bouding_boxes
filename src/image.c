ff242409445359df6f30e6b436bcf5aba23c5c97
Michael Schwanzer <michael.schwanzer@gmail.com>
2018-04-21 22:43:06 -0400

    forgot colon

--- src/image.c
+++ src/image.c
@@ -253,7 +253,7 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
                     strcat(labelstr, names[j]);
                 }
                 //printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
-                printf("%s:%.0f%%", names[j], dets[i].prob[j]*100);
+                printf("%s:%.0f%%:", names[j], dets[i].prob[j]*100);
             }
         }
         if(class >= 0){
